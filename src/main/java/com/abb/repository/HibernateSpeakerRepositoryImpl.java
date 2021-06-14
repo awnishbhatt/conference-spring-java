8774c3ea63d24a2a50c7a46111c3f50cc78f85e8
a b <forpluralsight1900@gmail.com>
2021-06-14 14:59:36 -0500

    spring profiles, postConstruct and BeanFactory

--- src/main/java/com/abb/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/abb/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,12 +1,14 @@
 package com.abb.repository;
 
 import com.abb.model.Speaker;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository("speakerRepository")
+@Profile("dev")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Override
     public List<Speaker> findAll() {
--- src/main/java/com/abb/service/SpeakerServiceImpl.java
+++ src/main/java/com/abb/service/SpeakerServiceImpl.java
@@ -4,12 +4,15 @@ import com.abb.model.Speaker;
 import com.abb.repository.HibernateSpeakerRepositoryImpl;
 import com.abb.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service("speakerService")
+@Profile("dev")
 public class SpeakerServiceImpl implements SpeakerService {
 
     public SpeakerRepository speakerRepository;
@@ -22,5 +25,10 @@ public class SpeakerServiceImpl implements SpeakerService {
     @Autowired
     public void setSpeakerRepository(SpeakerRepository speakerRepository) {
         this.speakerRepository = speakerRepository;
+    }
+
+    @PostConstruct
+    private void initialize(){
+        System.out.println("SpeakerServiceImpl:: post construct called after the constructor");
     }
 }
